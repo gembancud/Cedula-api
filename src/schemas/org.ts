772897b6d4772de81973fe1e0f6b68a0c0ae334b
Gil Emmanuel Bancud <gembancud@gmail.com>
2022-11-17 17:10:58 +0800

    fix: renamed badge in Org model to default_badge_link

--- src/schemas/org.ts
+++ src/schemas/org.ts
@@ -1,11 +1,10 @@
 import { Schema } from "mongoose";
 
-
 export interface Org {
   _id: string;
   name: string;
   image: string;
-  badge: string;
+  default_badge_link: string;
   description: string;
   createdAt: Date;
   access: string;
@@ -15,7 +14,7 @@ export interface Org {
 export const OrgSchema = new Schema<Org>({
   name: { type: String, required: true, index: true, unique: true },
   image: { type: String, required: true },
-  badge: { type: String, required: true },
+  default_badge_link: { type: String, required: true },
   description: { type: String },
   createdAt: { type: Date, default: Date.now },
   access: { type: String, required: true },
